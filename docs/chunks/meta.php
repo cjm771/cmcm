09bb23cf68b09ab1bf67734de45660cf2db176f3
cjm771 <chrisishere@gmail.com>
2014-05-02 07:35:53 -0400

    ico :3

--- docs/chunks/meta.php
+++ docs/chunks/meta.php
@@ -1,3 +1,4 @@
 		<meta name='description' content='CMCM is a JSON-text based content manager for designers, created to be easy, flexible, and simple. Use it as a backend for your portfolio, blog, or personal website.'> 
-		<meta property="og:image" content="http://cmcm.io/images/cmcm-icon-black.jpg" />
-		<meta name="thumbnail" content="http://cmcm.io/images/graphics_pack/cmcm-icon-black.jpg" />
+		<link rel="icon" href="http://cmcm.io/images/graphics_pack/CMCM.ico" type="image/x-icon">
+		<meta property="og:image" content="http://cmcm.io/images/graphics_pack/CMCM-thumb-black.jpg" />
+		<meta name="thumbnail" content="http://cmcm.io/images/graphics_pack/CMCM-thumb-black.jpg" />
